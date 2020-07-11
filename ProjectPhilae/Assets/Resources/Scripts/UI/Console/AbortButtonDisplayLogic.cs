dd0b8ae6decc1faa20dc9eda675799e849590425
David <david.rj.forster+gh@gmail.com>
2020-07-11 22:20:13 +0100

    Abort button

---
+++ ProjectPhilae/Assets/Resources/Scripts/UI/Console/AbortButtonDisplayLogic.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using static GameManagerController;
+
+public class AbortButtonDisplayLogic : MonoBehaviour
+{
+    public GameObject Button;
+
+    void Update()
+    {
+        Button.SetActive(Instance.State != GameStates.EnterInstructions);
+    }
+}
---
+++ ProjectPhilae/Assets/Resources/Scripts/UI/Console/ExecuteButtonDisplayLogic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using static GameManagerController;
+
+public class ExecuteButtonDisplayLogic : MonoBehaviour
+{
+    public GameObject Button;
+
+    void Update()
+    {
+        Button.SetActive(Instance.State == GameStates.EnterInstructions);
+    }
+}
