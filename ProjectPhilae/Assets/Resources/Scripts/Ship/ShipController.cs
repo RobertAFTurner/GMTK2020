303ef6eeeea1cf7c1f2e668c6c625b942375d02e
RobertAFTurner <robert_afturner@hotmail.com>
2020-07-11 08:51:27 +0100

    Add ship and add spawn point

---
+++ ProjectPhilae/Assets/Resources/Scripts/Ship/ShipController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    // CurrentControl
+    // ControlLisy
+
+    [SerializeField]
+    private Rigidbody2D rigidbody;
+
+    [SerializeField]
+    private float speed;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+            speed += 0.1f;
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+            speed -= 0.1f;
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(rigidbody.transform.up * speed);
+    }
+}
---
+++ ProjectPhilae/Assets/Resources/Scripts/Spawner/PlayerSpawnPointController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnPointController : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject playerShip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(playerShip, transform.position, Quaternion.identity);
+    }
+}
