ccc17b04193822fdf746dc65c43b581709917745
David <david.rj.forster+gh@gmail.com>
2020-07-11 11:11:23 +0100

    Basic command display

--- ProjectPhilae/Assets/Resources/Scripts/Commands/WaitCommand.cs
+++ ProjectPhilae/Assets/Resources/Scripts/Commands/WaitCommand.cs
@@ -1,7 +1,14 @@
 public class WaitCommand : ICommand
 {
-  public bool ExecuteTillDone()
-  {
-    return true;
-  }
+    public float Duration;
+
+    public bool ExecuteTillDone()
+    {
+        return true;
+    }
+
+    public override string ToString()
+    {
+        return $"Waiting for {Duration} seconds";
+    }
 }
---
+++ ProjectPhilae/Assets/Resources/Scripts/UI/Console/CommandDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class CommandDisplay : MonoBehaviour
+{
+    public ConsoleController ConsoleController;
+    private TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = this.GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var output = $"{string.Join(Environment.NewLine, ConsoleController.committedCommands)}{Environment.NewLine}>_";
+
+        text.SetText(output);
+    }
+}
