e353ccbbb48d6654f4690192e0c17a458c866dcc
David <david.rj.forster+gh@gmail.com>
2020-07-11 13:41:11 +0100

    Wait command
    Panel + execution

--- ProjectPhilae/Assets/Resources/Scripts/UI/Console/Nodes/WaitNodeController.cs
+++ ProjectPhilae/Assets/Resources/Scripts/UI/Console/Nodes/WaitNodeController.cs
@@ -1,19 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-public class ThrustNodeController : NodeBase<ThrustCommand>
+public class WaitNodeController : NodeBase<WaitCommand>
 {
-    [SerializeField]
-    private Slider slider;
-
     [SerializeField]
     private TMP_InputField input;
     
-    public void SetPower()
-    {
-        command.Power = slider.GetComponent<Slider>().value;
-    }
-
     public void SetDuration()
     {
         command.Duration = string.IsNullOrWhiteSpace(input.text) ? 0f : float.Parse(input.text);
