9b20b2a198745ce7d641a9841f68789c81bf8295
David <david.rj.forster+gh@gmail.com>
2020-07-12 14:35:56 +0100

    Display score stars!

---
+++ ProjectPhilae/Assets/Resources/Scripts/UI/StarDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+    private int starCount;
+    private int displayedStars;
+    private float timeStep = 0.5f;
+    private float lastTimeStep;
+    private RectTransform image;
+
+    public void DisplayStars(int stars)
+    {
+        displayedStars = 0;
+        starCount = stars;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<RectTransform>();
+        image.sizeDelta = new Vector2(0f, 100f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (displayedStars < starCount && Time.time - lastTimeStep > timeStep)
+        {
+            displayedStars++;
+            image.sizeDelta = new Vector2(displayedStars * 100f, 100f);
+            lastTimeStep = Time.time;
+        }
+    }
+}
