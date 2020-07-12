53941491d30d973b8937cc0b72cd0b63203ecdbf
David <david.rj.forster+gh@gmail.com>
2020-07-12 13:41:34 +0100

    Level data

---
+++ ProjectPhilae/Assets/Resources/Scripts/LevelData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData : MonoBehaviour
+{
+    public enum Commands
+    {
+        Thrust,
+        Wait,
+        Rotate,
+        Reverse,
+        Stop
+    }
+
+    public float FiveStarScore;
+    public float OneStarScore;
+    public float StartingFuel = 100f;
+
+    public List<Commands> ExcludedCommands;
+}
